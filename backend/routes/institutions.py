19288fc22ac23723483f2713127840a781d7f08a
SHaruto0 <szkharuto0407@gmail.com>
2025-11-08 01:04:32 -0500

    Add Required fields data structure

--- backend/routes/institutions.py
+++ backend/routes/institutions.py
@@ -3,24 +3,11 @@ from config.supabase import supabase
 
 institutions_bp = Blueprint('institutions', __name__)
 
+REQUIRED_FIELDS = {
+    "name": str,
+    "city": str,
+    "zip": str,
+    "cost": str,
+    "acceptance_rate": str
+}
 
-@institutions_bp.route('/institutions', methods=['GET', 'POST', 'PUT', 'DELETE'])
-def institutions():
-    """
-    Handle CRUD operations for institutions
-    """
-    if request.method == 'GET':
-        # TODO: Implement GET logic
-        pass
-    
-    elif request.method == 'POST':
-        # TODO: Implement POST logic
-        pass
-    
-    elif request.method == 'PUT':
-        # TODO: Implement PUT logic
-        pass
-    
-    elif request.method == 'DELETE':
-        # TODO: Implement DELETE logic
-        pass
