9b30fae1f2c924f2a53e6478c04cea4316468da5
Elizabeth Woo <ellewoo12@gmail.com>
2025-11-07 19:22:14 -0500

    created schema for college cards

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import NavBar from './components/navbar'
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import NavBar from "./components/navbar";
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import "./App.css";
+import CollegeContainer from "./containers/collegeContainer";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const links = ['CLEP Search', 'Recent Events']
+  const [count, setCount] = useState(0);
+  const links = ["CLEP Search", "Recent Events"];
 
   return (
     <>
       <Router>
         <NavBar links={links} />
-        <p className='text-blue-50'>Hello</p>
+        <p className="text-blue-50">Hello</p>
         <Routes></Routes>
-
       </Router>
+      <CollegeContainer />
     </>
-  )
+  );
 }
 
 export default App;
