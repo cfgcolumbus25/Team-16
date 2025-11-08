67d85ab414bdf7b477c1cb7b57002d333daed6ac
Karan5352 <karan.tandra@gmail.com>
2025-11-07 19:40:16 -0500

    Updated map - template

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import NavBar from "./components/navbar";
 import {
   HashRouter as Router,
   Routes,
-  Route,
-  Navigate,
 } from "react-router-dom";
 import "./App.css";
-import CollegeContainer from "./containers/collegeContainer";
+import CollegeContainer from "./containers/CollegeContainer";
 
 function App() {
-  const [count, setCount] = useState(0);
   const links = ["CLEP Search", "Recent Events"];
 
   return (
