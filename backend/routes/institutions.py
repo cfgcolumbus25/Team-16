780283f34cb1cde844eccfb87277ad6586e1cfef
SHaruto0 <szkharuto0407@gmail.com>
2025-11-08 01:07:21 -0500

    Add Get Method for Institutions

--- backend/routes/institutions.py
+++ backend/routes/institutions.py
@@ -11,3 +11,10 @@ REQUIRED_FIELDS = {
     "acceptance_rate": str
 }
 
+@institutions_bp.route('/institutions', methods=['GET'])
+def get_institutions():
+    try:
+        data = supabase.table("universities").select("*").order("name", desc=False).execute()
+        return jsonify(data.data), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
